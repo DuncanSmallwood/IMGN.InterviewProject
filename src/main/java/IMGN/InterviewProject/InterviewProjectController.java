d85f1f2e659f301b674ee8c59fa68425e6db361b
Duncan Smallwood <duncan@jackstudios.co.za>
2022-02-15 19:16:21 +0200

    Created InterviewProjectController and implemented constructor

---
+++ src/main/java/IMGN/InterviewProject/InterviewProjectController.java
@@ -0,0 +1,24 @@
+package IMGN.InterviewProject;
+
+import IMGN.InterviewProject.Repositories.Entities.Company;
+import IMGN.InterviewProject.Repositories.Entities.Employee;
+import IMGN.InterviewProject.Repositories.ICompanyRepository;
+import IMGN.InterviewProject.Repositories.IEmployeeRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InterviewProjectController {
+    private final ICompanyRepository companyRepository;
+    private final IEmployeeRepository employeeRepository;
+
+    InterviewProjectController(ICompanyRepository cRepo, IEmployeeRepository eRepo){
+        companyRepository = cRepo;
+        employeeRepository = eRepo;
+    }
+
+    //put/create company/employee
+    //get employee by company
+    //delete one employee
+}
